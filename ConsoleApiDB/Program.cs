6c5deb8cf9511a8de969df7cf0b6763d0230334b
Lil s0uxz <its.s0ux@gmail.com>
2023-03-07 11:45:53 +0700

    Add project files.

---
+++ ConsoleApiDB/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Owin.Hosting;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ConsoleApiDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StartTopshelf();
+
+            // Localhost
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.WriteLine(" /-------------------------------------------------/");
+                Console.WriteLine(" / Web server is running at http://localhost:12345 /");
+                Console.WriteLine(" /                                                 /");
+                Console.WriteLine(" / Press Ctrl + C to quit.                         /");
+                Console.WriteLine(" /-------------------------------------------------/");
+                Console.ReadLine();
+            }        
+        }
+
+        // Topshelf
+        //static void StartTopshelf()
+        //{
+        //    HostFactory.Run(x =>
+        //    {
+        //        x.Service<WebServer>(s =>
+        //        {
+        //            s.ConstructUsing(name => new WebServer());
+        //            s.WhenStarted(tc => tc.Start());
+        //            s.WhenStopped(tc => tc.Stop());
+        //        });
+        //        x.RunAsLocalSystem();
+
+        //        x.SetDescription("This is a demo of a Windows Service using Topshelf.");
+        //        x.SetDisplayName("Self Host Web API Demo");
+        //        x.SetServiceName("AspNetSelfHostDemo");
+        //    });
+        //}
+    }
+}
---
+++ ConsoleApiDB/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+
+namespace ConsoleApiDB
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Adding to the pipeline with our own middleware
+            app.Use(async (context, next) =>
+            {
+                // Add Header
+                context.Response.Headers["Product"] = "Web Api v1";
+
+                // Call next middleware
+                await next.Invoke();
+            });
+
+            // Custom Middleare
+            app.Use(typeof(CustomMiddleware));
+
+            // Configure Web API for self-host.
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "Api-1",
+                routeTemplate: "api/{controller}/{name}",
+                defaults: new { id = RouteParameter.Optional }
+               );
+            config.Routes.MapHttpRoute(
+                name: "Api-2",
+                routeTemplate: "api/{controller}/{name}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+             );
+
+            // Web Api
+            app.UseWebApi( config );
+        }
+    }
+}
